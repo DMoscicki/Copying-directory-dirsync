fb28047fdc8b32ce16bcf9d984f239eb7f2e3e98
DMoscicki <89485375+DMoscicki@users.noreply.github.com>
2021-08-24 23:19:57 +0300

    Add files via upload

---
+++ coping_directory.py
@@ -0,0 +1,26 @@
+import shutil
+from dirsync import sync
+import sys
+
+print(f'Введите путь к папке:')
+folder_1 = str(input())
+print(f'Введите путь куда хотите скопировать:')
+folder_2 = str(input())
+
+origin_stdout = sys.stdout
+sys.stdout = open('info.txt', 'w', encoding='utf-8')
+sync(folder_1, folder_2,'sync', verbose=True, purge=True)
+sys.stdout = origin_stdout
+
+with open('info.txt', 'r', encoding='utf-8') as file:
+    for line in file.readlines():
+        print(line, end='')
+
+# f = open('info.txt', 'r')
+# print(f.readlines())
+# f.close()
+# with open('info.txt', 'r') as file:
+#     for message in file:
+#         print(message, end='')
+# sys.stdout.close()
+# file.close()
